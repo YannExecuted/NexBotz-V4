06af0533f084656a4125c929dc51c137fb6e8b6d
YannExecuted <108313193+YannExecuted@users.noreply.github.com>
2022-12-30 16:42:27 +0700

    Update owner-botoff.js

--- plugins/owner-botoff.js
+++ plugins/owner-botoff.js
@@ -4,7 +4,7 @@ let handler = async (m, { participants }) => {
     m.reply(' *Sukses mematikan Bot 📴* ')
     // } else m.reply('Ada nomor Ownerku disini...')
 }
-handler.help = ['NexBotz(on/off)']
+handler.help = ['AkenoBotz(on/off)']
 handler.tags = ['owner']
 handler.command = /^(botoff)$/i
 
