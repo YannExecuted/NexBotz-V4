ab9265cd0867984584a7484b08730dc5edd9a97e
YannExecuted <108313193+YannExecuted@users.noreply.github.com>
2022-12-30 16:15:38 +0700

    Update sticker-colong.js

--- plugins/sticker-colong.js
+++ plugins/sticker-colong.js
@@ -9,8 +9,8 @@ try {
     if (/image|video/.test(mime)) {
       let img = await q.download()
       if (!img) throw 'Reply stiker nya!'
-      stiker = await sticker(img, false, 'By', 'NexBotz')
-    } else if (args[0]) stiker = await sticker(false, args[0], 'By', 'NexEp')
+      stiker = await sticker(img, false, 'By', 'AkenoBotz')
+    } else if (args[0]) stiker = await sticker(false, args[0], 'By', 'YatzModz')
   } finally {
     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     else throw 'Conversion failed'
