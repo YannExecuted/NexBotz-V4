7052b79389baefaf71384e9d7b4af0afcab8d82a
YannExecuted <108313193+YannExecuted@users.noreply.github.com>
2022-12-30 23:36:16 +0700

    Update tag-owner.js

--- plugins/tag-owner.js
+++ plugins/tag-owner.js
@@ -4,7 +4,7 @@ let handler  = async (m, { conn }) => {
 let tag = fs.readFileSync('./sticker/tag.webp')
 conn.sendFile(m.chat, tag, '', '', m)
 }
-handler.customPrefix = /^(@6282214729677)$/i
+handler.customPrefix = /^(@6282117289550)$/i
 handler.command = new RegExp
 
 export default handler
