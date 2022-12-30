3a61e8f5d3fa277b7ed3db8b33a7181c3c943ea7
YannExecuted <108313193+YannExecuted@users.noreply.github.com>
2022-12-30 16:30:25 +0700

    Update bot off.js

--- plugins/bot off.js
+++ plugins/bot off.js
@@ -1,12 +1,12 @@
 let handler = async (m, { participants }) => {
     // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply(' *Yaudah kalo gitu, Kanna mau tidur dulu kak🥱😴* ')
+    m.reply(' *Yaudah kalo gitu, Akeno mau tidur dulu kak🥱😴* ')
     // } else m.reply('Ada nomor Ownerku disini...')
 }
-handler.help = ['Kanna(on/off)']
+handler.help = ['Akeno(on/off)']
 handler.tags = ['main']
-handler.command = /^(kannaoff)$/i
+handler.command = /^(akenooff)$/i
 
 handler.admin = true
 
