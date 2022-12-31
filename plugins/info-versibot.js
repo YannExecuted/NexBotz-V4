af995a117940afb4897e145ac7f6a56f22f1f5df
YannExecuted <108313193+YannExecuted@users.noreply.github.com>
2022-12-31 11:23:45 +0700

    Update info-versibot.js

--- plugins/info-versibot.js
+++ plugins/info-versibot.js
@@ -8,7 +8,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
     const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
     const detek = Math.floor( Kurang % (1000 * 60) / 1000)
-  let info = `Bot ini menggunakan script by\n ᴹᴿ᭄ YatzModz ×፝֟͜×\n\n*Version:* Private Edition V4`
+  let info = `Bot ini menggunakan script by\n ᴹᴿ᭄ PutaModz ×፝֟͜×\n\n*Version:* Private Edition V4`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 const message = {
