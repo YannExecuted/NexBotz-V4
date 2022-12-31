4254564dc0ff8f5088379acd9c168585acf5063e
YannExecuted <108313193+YannExecuted@users.noreply.github.com>
2022-12-31 11:37:09 +0700

    Update rpg-merampok.js

--- plugins/rpg-merampok.js
+++ plugins/rpg-merampok.js
@@ -9,10 +9,10 @@ let nomors = m.sender
   if (!who) throw 'Tag salah satu lah'
   if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
   let __timers = (new Date - global.db.data.users[m.sender].lastrob)
-  let _timers = (3600000 - __timers) 
+  let _timers = (1800000 - __timers) 
   let timers = clockString(_timers)
   let users = global.db.data.users
-  if (new Date - global.db.data.users[m.sender].lastrob > 3600000){
+  if (new Date - global.db.data.users[m.sender].lastrob > 1800000){
   if (10000 > users[who].money) throw 'ᴛᴀʀɢᴇᴛ ɢᴀᴀᴅᴀ 💰ᴜᴀɴɢ ʙᴏᴅᴏʜ, ᴋɪꜱᴍɪɴ ᴅɪᴀ'
   users[who].money -= dapat * 1
   users[m.sender].money += dapat * 1
